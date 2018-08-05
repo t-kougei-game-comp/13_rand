4986ba26502f2e041932a3b27d179bab65d63dc0
Takashi Imagire <imagire@gmail.com>
2018-08-05 20:43:46 +0900

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,22 @@ unsigned int my_rand()
     return g_last;
 }
 
+#define a 48271
+#define m 2147483647
+#define q (m / a)
+#define r (m % a)
+
+long int PMrand()
+{
+	long int hi = g_last / q;
+	long int lo = g_last % q;
+	long int test = a * lo - r * hi;
+	if(test > 0)
+		g_last = test;
+	else	g_last = test + m;
+	return g_last;
+}
+
 int load_data()
 {
     char str[100];
@@ -25,7 +41,7 @@ int main(int argc, char *argv[])
     g_last = seed;
     
     for(int i = 0; i < num; i++){
-        unsigned int x = my_rand();
+        unsigned int x = PMrand();
         unsigned int val = x / (0xfffffffe/max + 1)+1;
         printf("%u\n", val);
     }
