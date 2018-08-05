df9222b3356f0e4943f45b602f5dd56f75422acb
Takashi Imagire <imagire@gmail.com>
2018-08-05 21:13:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
     for(int i = 0; i < num; i++){
         unsigned int x = PMrand();
         unsigned int val = x / (0x7fffffff/max)+1;
-        printf("%u\n", val);
+        printf("%u\n", x);
     }
     
     return 0;
