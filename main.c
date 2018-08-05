c67ba4d4bd2f3aca79ba197062ffbb503de45ec8
Takashi Imagire <imagire@gmail.com>
2018-08-05 22:10:53 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,24 +4,8 @@
 static int g_last = 0;
 unsigned int my_rand()
 {
-    g_last = ((unsigned int)48271 * g_last) % 0x7fffffff;
+    g_last = (48271 * (long long int)g_last) % 0x7fffffff;
     return g_last;
-}
-
-#define a 48271
-#define m 0x7fffffff
-#define q (m / a)
-#define r (m % a)
-
-long int PMrand()
-{
-	int hi = g_last / q;
-	int lo = g_last % q;
-	int test = a * lo - r * hi;
-	if(test > 0)
-		g_last = test;
-	else	g_last = test + m;
-	return g_last;
 }
 
 int load_data()
