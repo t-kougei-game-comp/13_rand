0fc4152290cb8d3ec0f6731958d40b197a6853db
Takashi Imagire <imagire@gmail.com>
2018-08-05 21:40:36 +0900

    Update main.c

--- main.c
+++ main.c
@@ -18,9 +18,9 @@ long int PMrand()
 	int hi = g_last / q;
 	int lo = g_last % q;
 	int test = a * lo - r * hi;
-//	if(test > 0)
+	if(test > 0)
 		g_last = test;
-//	else	g_last = test + m;
+	else	g_last = test + m;
 	return g_last;
 }
 
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
     g_last = seed;
     
     for(int i = 0; i < num; i++){
-        int x = PMrand();
+        int x = PMrand()-1;
         int val = x / (0x7fffffff/max)+1;
 	printf("%d\n", val);
     }
